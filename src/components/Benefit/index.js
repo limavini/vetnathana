819dc550bce7eaf343026a2eec66273709aeac0a
limavini <viniciusalmeidait@gmail.com>
2020-10-02 16:21:36 -0300

    completing benefits

--- src/components/Benefit/index.js
+++ src/components/Benefit/index.js
@@ -1,12 +1,17 @@
 import React from "react"
-import { AiOutlineHome } from "react-icons/ai"
+import "./styles.scss"
 
 export const Benefit = ({ children, text }) => {
   return (
-    <div className="flex justify-center flex-col items-center">
-      <div className="pb-3"></div>
-
-      <div>{text}</div>
+    <div className="flex justify-center flex-col items-center pb-3 item px-3">
+      {children}
+      <div className="pt-8 text-gray-900 font-semibold pb-3 text-center ">
+        {text.toUpperCase()}
+      </div>
+      <div className="text-gray-500 text-center lg:px-12 sm:p-0">
+        É imperioso ressaltar que o princípio suscetível que serve de base vela
+        a enunciação sem que isso impeça a extração de gozo.
+      </div>
     </div>
   )
 }
--- src/components/Benefits/index.js
+++ src/components/Benefits/index.js
@@ -1,20 +1,36 @@
 import React from "react"
 import { Benefit } from "../Benefit"
-import { AiOutlineHome } from "react-icons/ai"
+import {
+  AiOutlineHeart,
+  AiOutlineHome,
+  AiOutlineMedicineBox,
+} from "react-icons/ai"
+import { GiTurtle } from "react-icons/gi"
+
 export const Benefits = () => {
+  const color = "#38a169"
+
   return (
     <div className="bg-white h-screen px-16 py-10">
-      <span className="text-gray-700 flex justify-center text-2xl pb-10">
-        OFERECEMOS O MELHOR SERVIÇO PARA SEU PET
-      </span>
-
-      <div className="flex justify-around">
-        <Benefit text="Atendimento a Domicílio">
-          <AiOutlineHome size={80} fill="#cbd5e0" />
+      <div className="flex justify-center">
+        <span className="text-gray-700 flex justify-center text-2xl pb-24 text-center w-6/12">
+          Segundo Heidegger, a infinita diversidade da realidade única nos
+          obriga à análise das condições epistemológicas e cognitivas exigidas
+        </span>
+      </div>
+      <div className="flex flex-wrap w-full flex-row">
+        <Benefit text="Capitais Internacionais">
+          <AiOutlineHome size={80} fill={color} />
         </Benefit>
-        <Benefit />
-        <Benefit />
-        <Benefit />
+        <Benefit text="Métodos Utilizados ">
+          <AiOutlineMedicineBox size={80} fill={color} />
+        </Benefit>
+        <Benefit text="Avaliações">
+          <GiTurtle size={80} fill={color} />
+        </Benefit>
+        <Benefit text="Mundo">
+          <AiOutlineHeart size={80} fill={color} />
+        </Benefit>
       </div>
     </div>
   )
